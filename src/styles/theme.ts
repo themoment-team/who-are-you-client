e4e019a984b1cae49008f19c917c2a3a8af18957
gjaegyun <jaegyungim06@gmail.com>
2024-05-10 11:29:47 +0900

    Add theme.ts

---
+++ src/styles/theme.ts
@@ -0,0 +1,67 @@
+const typo = {
+  h1: {
+    fontSize: '1.5rem',
+    lineHeight: '2rem',
+    fontWeight: 500,
+  },
+  h2: {
+    fontSize: '1.375rem',
+    lineHeight: '1.75rem',
+    fontWeight: 500,
+  },
+  h3: {
+    fontSize: '1.125rem',
+    lineHeight: '1.625rem',
+    fontWeight: 500,
+  },
+  title: {
+    fontSize: '1.75rem',
+    lineHeight: '2.75rem',
+    fontWeight: '500',
+  },
+  subtitle: {
+    fontSize: '1rem',
+    lineHeight: '1.5rem',
+    fontWeight: 600,
+  },
+  description: {
+    fontSize: '0.875rem',
+    lineHeight: '1.375rem',
+    fontWeight: 500,
+  },
+  caption: {
+    fontSize: '0.75rem',
+    lineHeight: '1.25rem',
+    fontWeight: 500,
+  },
+};
+
+const color = {
+  primary: {
+    10: '#D8D4FF',
+    20: '#AFA8FF',
+    30: '#6052FF',
+    40: '#1000C2',
+  },
+  gray: {
+    10: '#F5F5F5',
+    20: '#E8E8E8',
+    30: '#CFCFCF',
+    40: '#BDBDBD',
+    50: '#999',
+    60: '#888',
+    70: '#777',
+    80: '#666',
+    90: '#333',
+  },
+  black: '#000',
+  white: '#FFFFFF',
+  error: '#F22424',
+};
+
+const theme = {
+  typo,
+  color,
+};
+
+export default theme;
