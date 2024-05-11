8f4994d8dfe20c5d97ea01bf96f838d270741c47
gaoooon <gaon9749@gmail.com>
2024-05-12 03:24:27 +0900

    Add GlobalStyle

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { ThemeProvider } from '@emotion/react';
 import theme from '@/styles/theme.ts';
+import GlobalStyle from '@/styles/GlobalStyle.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ThemeProvider theme={theme}>
+    <GlobalStyle />
     <App />
   </ThemeProvider>
 );
