9dbb745f425bc0e1597f011a8d7d4fd0ae97bdbd
gaoooon <gaon9749@gmail.com>
2024-05-12 16:33:19 +0900

    Update path

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { ThemeProvider } from '@emotion/react';
-import theme from '@/styles/theme.ts';
-import GlobalStyle from '@/styles/GlobalStyle.tsx';
+import { theme, GlobalStyle } from '@/styles';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ThemeProvider theme={theme}>
