a684cd9d674f5afac536b14a23f4fa53fa539b9f
gaoooon <gaon9749@gmail.com>
2024-05-14 00:32:01 +0900

    Add CameraGuide.tsx

---
+++ src/assets/CameraGuide.tsx
@@ -0,0 +1,61 @@
+const CameraGuide = () => (
+  <svg
+    width='32.75rem'
+    height='26.8125rem'
+    viewBox='0 0 524 429'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <path
+      fillRule='evenodd'
+      clipRule='evenodd'
+      d='M524 0H0V429H524V0ZM262.5 380C353.903 380 428 305.903 428 214.5C428 123.097 353.903 49 262.5 49C171.097 49 97 123.097 97 214.5C97 305.903 171.097 380 262.5 380Z'
+      fill='black'
+      fillOpacity='0.5'
+    />
+    <mask id='path-2-inside-1_25_264' fill='white'>
+      <path d='M20 20H44V44H20V20Z' />
+    </mask>
+    <path
+      d='M20 20V18H18V20H20ZM20 22H44V18H20V22ZM22 44V20H18V44H22Z'
+      fill='white'
+      mask='url(#path-2-inside-1_25_264)'
+    />
+    <mask id='path-4-inside-2_25_264' fill='white'>
+      <path d='M20 409L20 385L44 385L44 409L20 409Z' />
+    </mask>
+    <path
+      d='M20 409L18 409L18 411L20 411L20 409ZM22 409L22 385L18 385L18 409L22 409ZM44 407L20 407L20 411L44 411L44 407Z'
+      fill='white'
+      mask='url(#path-4-inside-2_25_264)'
+    />
+    <mask id='path-6-inside-3_25_264' fill='white'>
+      <path d='M504 409L480 409L480 385L504 385L504 409Z' />
+    </mask>
+    <path
+      d='M504 409L504 411L506 411L506 409L504 409ZM504 407L480 407L480 411L504 411L504 407ZM502 385L502 409L506 409L506 385L502 385Z'
+      fill='white'
+      mask='url(#path-6-inside-3_25_264)'
+    />
+    <mask id='path-8-inside-4_25_264' fill='white'>
+      <path d='M504 20L504 44L480 44L480 20L504 20Z' />
+    </mask>
+    <path
+      d='M504 20L506 20L506 18L504 18L504 20ZM502 20L502 44L506 44L506 20L502 20ZM480 22L504 22L504 18L480 18L480 22Z'
+      fill='white'
+      mask='url(#path-8-inside-4_25_264)'
+    />
+    <circle
+      cx='261.5'
+      cy='214.5'
+      r='165.5'
+      stroke='white'
+      strokeWidth='4'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeDasharray='10 10'
+    />
+  </svg>
+);
+
+export default CameraGuide;
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,1 +1,2 @@
 export { default as WhoAreYouLogo } from './WhoAreYouLogo';
+export { default as CameraGuide } from './CameraGuide';
