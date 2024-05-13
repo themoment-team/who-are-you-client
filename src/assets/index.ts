41b67e0b60f821ada69d5effda41c1b433330da1
gaoooon <gaon9749@gmail.com>
2024-05-14 08:58:07 +0900

    Add O, X icon

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,2 +1,4 @@
 export { default as WhoAreYouLogo } from './WhoAreYouLogo';
 export { default as CameraGuide } from './CameraGuide';
+export { default as OIcon } from './OIcon';
+export { default as Xicon } from './Xicon';
