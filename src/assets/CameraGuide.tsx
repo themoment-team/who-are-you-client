699afefe13456dca848f7df00fba3e70a3152c35
gaoooon <gaon9749@gmail.com>
2024-05-14 08:57:25 +0900

    Update CameraGuide.tsx

--- src/assets/CameraGuide.tsx
+++ src/assets/CameraGuide.tsx
@@ -1,60 +1,101 @@
 const CameraGuide = () => (
   <svg
     width='32.75rem'
-    height='26.8125rem'
-    viewBox='0 0 524 429'
+    height='24.5625rem'
+    viewBox='0 0 524 393'
     fill='none'
     xmlns='http://www.w3.org/2000/svg'
   >
-    <path
-      fillRule='evenodd'
-      clipRule='evenodd'
-      d='M524 0H0V429H524V0ZM262.5 380C353.903 380 428 305.903 428 214.5C428 123.097 353.903 49 262.5 49C171.097 49 97 123.097 97 214.5C97 305.903 171.097 380 262.5 380Z'
-      fill='black'
-      fillOpacity='0.5'
-    />
-    <mask id='path-2-inside-1_25_264' fill='white'>
-      <path d='M20 20H44V44H20V20Z' />
-    </mask>
-    <path
-      d='M20 20V18H18V20H20ZM20 22H44V18H20V22ZM22 44V20H18V44H22Z'
-      fill='white'
-      mask='url(#path-2-inside-1_25_264)'
-    />
-    <mask id='path-4-inside-2_25_264' fill='white'>
-      <path d='M20 409L20 385L44 385L44 409L20 409Z' />
-    </mask>
-    <path
-      d='M20 409L18 409L18 411L20 411L20 409ZM22 409L22 385L18 385L18 409L22 409ZM44 407L20 407L20 411L44 411L44 407Z'
-      fill='white'
-      mask='url(#path-4-inside-2_25_264)'
-    />
-    <mask id='path-6-inside-3_25_264' fill='white'>
-      <path d='M504 409L480 409L480 385L504 385L504 409Z' />
-    </mask>
-    <path
-      d='M504 409L504 411L506 411L506 409L504 409ZM504 407L480 407L480 411L504 411L504 407ZM502 385L502 409L506 409L506 385L502 385Z'
-      fill='white'
-      mask='url(#path-6-inside-3_25_264)'
-    />
-    <mask id='path-8-inside-4_25_264' fill='white'>
-      <path d='M504 20L504 44L480 44L480 20L504 20Z' />
-    </mask>
-    <path
-      d='M504 20L506 20L506 18L504 18L504 20ZM502 20L502 44L506 44L506 20L502 20ZM480 22L504 22L504 18L480 18L480 22Z'
-      fill='white'
-      mask='url(#path-8-inside-4_25_264)'
-    />
-    <circle
-      cx='261.5'
-      cy='214.5'
-      r='165.5'
-      stroke='white'
-      strokeWidth='4'
-      strokeLinecap='round'
-      strokeLinejoin='round'
-      strokeDasharray='10 10'
-    />
+    <g clipPath='url(#clip0_62_21)'>
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M524 0H0V393H524V0ZM262.5 345C344.514 345 411 278.514 411 196.5C411 114.486 344.514 48 262.5 48C180.486 48 114 114.486 114 196.5C114 278.514 180.486 345 262.5 345Z'
+        fill='black'
+        fillOpacity='0.5'
+      />
+      <mask
+        id='mask0_62_21'
+        style={{ maskType: 'luminance' }}
+        maskUnits='userSpaceOnUse'
+        x='20'
+        y='20'
+        width='1.5rem'
+        height='1.5rem'
+      >
+        <path d='M20 20H44V44H20V20Z' fill='white' />
+      </mask>
+      <g mask='url(#mask0_62_21)'>
+        <path
+          d='M20 20V18H18V20H20ZM20 22H44V18H20V22ZM22 44V20H18V44H22Z'
+          fill='white'
+        />
+      </g>
+      <mask
+        id='mask1_62_21'
+        style={{ maskType: 'luminance' }}
+        maskUnits='userSpaceOnUse'
+        x='20'
+        y='349'
+        width='1.5rem'
+        height='1.5rem'
+      >
+        <path d='M20 373V349H44V373H20Z' fill='white' />
+      </mask>
+      <g mask='url(#mask1_62_21)'>
+        <path
+          d='M20 373H18V375H20V373ZM22 373V349H18V373H22ZM44 371H20V375H44V371Z'
+          fill='white'
+        />
+      </g>
+      <mask
+        id='mask2_62_21'
+        style={{ maskType: 'luminance' }}
+        maskUnits='userSpaceOnUse'
+        x='480'
+        y='349'
+        width='1.5rem'
+        height='1.5rem'
+      >
+        <path d='M504 373H480V349H504V373Z' fill='white' />
+      </mask>
+      <g mask='url(#mask2_62_21)'>
+        <path
+          d='M504 373V375H506V373H504ZM504 371H480V375H504V371ZM502 349V373H506V349H502Z'
+          fill='white'
+        />
+      </g>
+      <mask
+        id='mask3_62_21'
+        style={{ maskType: 'luminance' }}
+        maskUnits='userSpaceOnUse'
+        x='480'
+        y='20'
+        width='1.5rem'
+        height='1.5rem'
+      >
+        <path d='M504 20V44H480V20H504Z' fill='white' />
+      </mask>
+      <g mask='url(#mask3_62_21)'>
+        <path
+          d='M504 20H506V18H504V20ZM502 20V44H506V20H502ZM480 22H504V18H480V22Z'
+          fill='white'
+        />
+      </g>
+      <path
+        d='M261.5 346C344.067 346 411 279.067 411 196.5C411 113.933 344.067 47 261.5 47C178.933 47 112 113.933 112 196.5C112 279.067 178.933 346 261.5 346Z'
+        stroke='white'
+        strokeWidth='4'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeDasharray='10 10'
+      />
+    </g>
+    <defs>
+      <clipPath id='clip0_62_21'>
+        <rect width='32.75rem' height='24.5625rem' rx='8' fill='white' />
+      </clipPath>
+    </defs>
   </svg>
 );
 
