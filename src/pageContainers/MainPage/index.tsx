a31d80efc1a159a0c676ca3e0ae9e63715f172f0
gaoooon <gaon9749@gmail.com>
2024-05-14 09:08:37 +0900

    Update  MainPage

--- src/pageContainers/MainPage/index.tsx
+++ src/pageContainers/MainPage/index.tsx
@@ -1,10 +1,19 @@
 import { Header } from '@/components';
 import * as S from './style';
+import { PhotoPage } from '@/pageContainers';
+import { useState } from 'react';
+import { Flow } from '@/types';
 
 const MainPage = () => {
+  const [flow, setFlow] = useState<Flow>(Flow.FORM_FLOW);
+  const [Image, setImage] = useState<File | null>(null);
+
   return (
     <S.Container>
       <Header />
+      {flow === Flow.PHOTO_FLOW && (
+        <PhotoPage setImage={setImage} setFlow={setFlow} />
+      )}
     </S.Container>
   );
 };
