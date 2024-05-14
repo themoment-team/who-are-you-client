bba7ad179f4d37e9d85f7a41207549778350d956
gjaegyun <jaegyungim06@gmail.com>
2024-05-14 14:50:30 +0900

    Add mbtiArray

---
+++ src/constants/mbtiArray.ts
@@ -0,0 +1,19 @@
+const MBTI_ARRAY = [
+  'ISTJ',
+  'ISFJ',
+  'INFJ',
+  'INTJ',
+  'ISTP',
+  'ISFP',
+  'INFP',
+  'INTP',
+  'ESTP',
+  'ESFP',
+  'ENFP',
+  'ENTP',
+  'ESTJ',
+  'ESFJ',
+  'ENFJ',
+  'ENTJ',
+] as const;
+export default MBTI_ARRAY;
