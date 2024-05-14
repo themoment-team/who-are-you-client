fd4782f8453128b75cb842e34bec73a3ba48ab92
gjaegyun <jaegyungim06@gmail.com>
2024-05-14 09:39:37 +0900

    Add mainPageFrame

--- src/pageContainers/MainPage/index.tsx
+++ src/pageContainers/MainPage/index.tsx
@@ -1,11 +1,100 @@
-import { Header } from '@/components';
 import * as S from './style';
 
+import type { SubmitErrorHandler, SubmitHandler } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import { Header, InputFormItem, SelectFormItem } from '@/components';
+import { userInfoFormSchema } from '@/schemas';
+import type { userInfoFormType } from '@/types';
+// import { useEffect } from 'react';
+
 const MainPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<userInfoFormType>({
+    resolver: zodResolver(userInfoFormSchema),
+    defaultValues: {
+      name: '',
+      phoneNumber: '',
+      email: '',
+      mbti: 'mbti를 선택해주세요.',
+    },
+  });
+
+  const onSubmit: SubmitHandler<userInfoFormType> = (data) => {
+    const body = {
+      ...data,
+      profileUrl: undefined,
+      generation: Number(data.mbti),
+    };
+
+    console.log(body);
+    console.log('hello');
+  };
+
+  const onError: SubmitErrorHandler<userInfoFormType> = () => {
+    // toast.error('입력 정보를 다시 확인해주세요.');
+    console.log('error');
+  };
+
+  // useEffect(() => {
+  //   toast.info('멘티 정보 등록 후에 서비스 이용이 가능합니다.');
+  // }, []);
+
   return (
-    <S.Container>
-      <Header />
-    </S.Container>
+    <S.Wrapper>
+      <S.Container>
+        <div>
+          <Header />
+          <S.TextContainer>
+            <S.TextBox>
+              <S.Text>
+                후아유에 오신걸 환영합니다! <br />
+                먼저, 명함에 들어갈 정보들을 입력해주세요.
+              </S.Text>
+            </S.TextBox>
+          </S.TextContainer>
+
+          <S.InputContainer onSubmit={handleSubmit(onSubmit, onError)}>
+            <InputFormItem
+              {...register('name')}
+              inputTitle='이름'
+              placeholder='이름을 입력해주세요.'
+              errorMessage={errors.name?.message}
+              required
+            />
+            <InputFormItem
+              {...register('phoneNumber')}
+              inputTitle='전화번호'
+              placeholder='전화번호를 입력해주세요.'
+              errorMessage={errors.phoneNumber?.message}
+              required
+            />
+            <InputFormItem
+              {...register('email')}
+              inputTitle='이메일'
+              placeholder='이메일을 입력해주세요.'
+              errorMessage={errors.email?.message}
+              required
+            />
+            <InputFormItem
+              inputTitle='전공/직함'
+              placeholder='전공/직함을 입력해주세요.'
+            />
+            <SelectFormItem selectTitle='MBTI' options={[]} />
+            <S.BottomContainer>
+              <S.ContinueButton>
+                <S.ContinueText>계속</S.ContinueText>
+              </S.ContinueButton>
+            </S.BottomContainer>
+          </S.InputContainer>
+        </div>
+      </S.Container>
+    </S.Wrapper>
   );
 };
 
--- src/pageContainers/MainPage/style.ts
+++ src/pageContainers/MainPage/style.ts
@@ -1,9 +1,72 @@
 import styled from '@emotion/styled';
 
-export const Container = styled.div`
+export const Wrapper = styled.div`
   background-color: ${({ theme }) => theme.color.white};
   width: 38.75rem;
   height: 100vh;
   height: 100dvh;
   margin: 0 auto;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 12.75rem;
+`;
+
+export const TextContainer = styled.div`
+  display: flex;
+  padding: 0rem 3rem;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 0.5rem;
+`;
+
+export const TextBox = styled.div`
+  display: flex;
+  padding: 2.5rem 2.5rem 2.25rem 0rem;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+export const Text = styled.p`
+  color: ${({ theme }) => theme.color.gray[90]};
+  ${({ theme }) => theme.typo.title};
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  padding: 0rem 3rem;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+export const BottomContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding-left: 24.875rem;
+  justify-content: flex-end;
+  gap: 0.5rem;
+  margin-top: 11.75rem;
+`;
+
+export const ContinueButton = styled.button`
+  display: flex;
+  width: 7.875rem;
+  height: 3rem;
+  padding: 0.625rem 1rem;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+  border-radius: 0.5rem;
+  background: ${({ theme }) => theme.color.primary[30]};
+`;
+
+export const ContinueText = styled.h3`
+  ${({ theme }) => theme.typo.h3};
+  color: ${({ theme }) => theme.color.white};
+  text-align: center;
 `;
