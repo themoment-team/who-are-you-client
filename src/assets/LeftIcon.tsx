ea43c79520a54dab7f58dd854361dcb74e175469
gjaegyun <jaegyungim06@gmail.com>
2024-05-15 22:01:31 +0900

    Add Icons

---
+++ src/assets/LeftIcon.tsx
@@ -0,0 +1,19 @@
+const LeftIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='25'
+    viewBox='0 0 24 25'
+    fill='none'
+  >
+    <path
+      d='M15.75 5L8.25 12.5L15.75 20'
+      stroke='black'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+  </svg>
+);
+
+export default LeftIcon;
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,3 +2,5 @@ export { default as WhoAreYouLogo } from './WhoAreYouLogo';
 export { default as CameraGuide } from './CameraGuide';
 export { default as OIcon } from './OIcon';
 export { default as Xicon } from './Xicon';
+export { default as LeftIcon } from './LeftIcon';
+export { default as RightIcon } from './RightIcon';
