27e4d4feb3717508f4f3697eef53a46d4bd606cd
gjaegyun <jaegyungim06@gmail.com>
2024-05-25 23:04:57 +0900

    Add GsmLogo

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -5,3 +5,4 @@ export { default as XIcon } from './XIcon';
 export { default as LeftIcon } from './LeftIcon';
 export { default as RightIcon } from './RightIcon';
 export { default as SelectIcon } from './SelectIcon';
+export { default as GsmLogo } from './GsmLogo';
