a47fa0ea4cb86873b5741809f2cdd8c41ff514cf
h-0y28 <s24015@gsm.hs.kr>
2024-10-23 00:23:51 +0900

    style: update design for Theme 2

--- src/components/CardTheme/Theme2/index.tsx
+++ src/components/CardTheme/Theme2/index.tsx
@@ -1,16 +1,14 @@
 import * as S from './style';
 import * as T from '../style';
 
-const GSM_LOGO_PATH = '/image/GSMLogo.png' as const;
+const LINE_IMG_PATH = '/image/LineImg.png' as const;
 
 interface Props {
   name: string;
-  instagram: string;
   imageUrl: string;
   phoneNumber?: string;
   email?: string;
   major?: string;
-  mbti?: string;
 }
 
 const Theme2: React.FC<Props> = ({
@@ -18,15 +16,11 @@ const Theme2: React.FC<Props> = ({
   major,
   phoneNumber,
   email,
-  mbti,
-  instagram,
   imageUrl,
 }) => {
   const userInfo = [
     { label: 'Tel', value: phoneNumber },
     { label: 'Email', value: email },
-    { label: 'MBTI', value: mbti },
-    { label: 'Insta', value: instagram },
   ].filter((info) => info.value);
 
   return (
@@ -34,7 +28,6 @@ const Theme2: React.FC<Props> = ({
       <S.ImgContinaer>
         <S.ImageWrapper>
           <S.ImgBox src={imageUrl} />
-          <S.LogoImage src={GSM_LOGO_PATH}/>
         </S.ImageWrapper>
       </S.ImgContinaer>
 
@@ -42,8 +35,8 @@ const Theme2: React.FC<Props> = ({
         <T.MainInfoBox>
           <T.UserName>{name}</T.UserName>
           <T.UserMajor>{major}</T.UserMajor>
+          <S.LineImage src={LINE_IMG_PATH} />
         </T.MainInfoBox>
-
         <T.UserInfoBox>
           {userInfo.map((info, index) => (
             <T.UserInfoText key={index}>
--- src/components/CardTheme/Theme2/style.ts
+++ src/components/CardTheme/Theme2/style.ts
@@ -3,20 +3,19 @@ import styled from '@emotion/styled';
 export const Contianer = styled.div`
   width: fit-content;
   display: flex;
-  padding: 2rem;
+  padding: 1.9rem;
   flex-direction: column;
   align-items: flex-start;
   justify-content: space-between;
   height: 23.5rem;
-  border-radius: 0.5rem;
-  border: 0.0625rem solid ${({ theme }) => theme.color.gray[40]};
+  border: 0.0625rem solid ${({ theme }) => theme.color.gray[30]};
   background-color: ${({ theme }) => theme.color.white};
 `;
 
 export const ImgContinaer = styled.div`
   display: flex;
   width: 11.3125rem;
-  justify-content: flex-end;
+  justify-content: flex-start;
   align-items: center;
   gap: 0.5rem;
 `;
@@ -32,25 +31,29 @@ export const UserContianer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  gap: 0.75rem;
+  gap: 2.5rem;
 `;
 
 export const ImgBox = styled.img`
   width: 5.25rem;
   height: 5.25rem;
-  border-radius: 0.5rem;
+  border-radius: 0.29rem;
   object-fit: cover;
 `;
-
 
 export const ImageWrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 0.375rem;
   align-items: flex-end;
-`
+`;
 
 export const LogoImage = styled.img`
   width: 4.5rem;
   height: 2.0625rem;
-`
+`;
+
+export const LineImage = styled.img`
+  width: 2.2rem;
+  margin-top: 0.4rem;
+`;
