a8dad3667639df35797ac4be58981615549967d0
junjuny0227 <junjuny0227@gmail.com>
2025-04-05 19:56:10 +0900

    fix: 공통 스타일 변경

--- src/components/CardTheme/style.ts
+++ src/components/CardTheme/style.ts
@@ -1,30 +1,34 @@
 import styled from '@emotion/styled';
 
-export const UserInfoBox = styled.div`
+export const MainInfoBox = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
-  gap: 0.125rem;
+  gap: 0.1875rem;
+  z-index: 1;
 `;
 
 export const UserName = styled.h1`
-  ${({ theme }) => theme.typo.h1};
-  color: ${({ theme }) => theme.color.gray[90]};
-  font-weight: bold;
+  margin: 0.25rem 0;
+  font-size: 1rem;
+  color: ${({ theme }) => theme.color.black};
+  font-weight: 600;
 `;
 
 export const UserMajor = styled.p`
   ${({ theme }) => theme.typo.description};
-  color: ${({ theme }) => theme.color.primary[30]};
+  color: ${({ theme }) => theme.color.gray[60]};
+  margin: 0.0625rem 0;
 `;
 
-export const UserInfoText = styled.p`
-  ${({ theme }) => theme.typo.caption};
-  color: ${({ theme }) => theme.color.gray[70]};
-`;
-
-export const MainInfoBox = styled.div`
+export const SubInfoBox = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
+  gap: 0.3125rem;
+`;
+
+export const SubInfoText = styled.p`
+  font-size: 0.5313rem;
+  font-weight: 400;
+  color: #494949;
+  margin: 0.0469rem 0;
 `;
