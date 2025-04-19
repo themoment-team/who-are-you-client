07abe463150af3e0e279fc17cf3227dbd4bf9cdf
junjuny0227 <junjuny0227@gmail.com>
2025-04-20 02:00:26 +0900

    feat: GSMLogoProps

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,4 @@ export * from './flow';
 export * from './selected';
 export * from './cardTheme';
 export * from './cardContent';
+export * from './logo';
---
+++ src/types/logo.ts
@@ -0,0 +1,4 @@
+export interface GSMLogoProps {
+  top?: string;
+  left?: string;
+}
